11ac7a86f75a2fa67199ede6ede34e480591a309
adarsh-s-nayak <pes1ug20cs620@pesu.pes.edu>
2023-10-28 21:29:22 +0530

    created home screen for Nextjs

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,75 +1,63 @@
-import { Text, useSx, View, H1, P, Row, A } from 'dripsy'
+import { getLatestNews } from "app/api/news";
+import { News } from "app/types/news";
+import { Text, useSx, View, H1, P, Row, A, FlatList, H2, Image } from 'dripsy'
+import { useEffect, useState } from 'react'
+import { ListRenderItem, Platform } from 'react-native'
 import { TextLink } from 'solito/link'
-import { MotiLink } from 'solito/moti'
 
 export function HomeScreen() {
   const sx = useSx()
 
-  return (
-    <View
-      sx={{ flex: 1, justifyContent: 'center', alignItems: 'center', p: 16 }}
-    >
-      <H1 sx={{ fontWeight: '800' }}>Welcome to Solito.</H1>
-      <View sx={{ maxWidth: 600 }}>
-        <P sx={{ textAlign: 'center' }}>
-          Here is a basic starter to show you how you can navigate from one
-          screen to another. This screen uses the same code on Next.js and React
-          Native.
-        </P>
-        <P sx={{ textAlign: 'center' }}>
-          Solito is made by{' '}
-          <A
-            href="https://twitter.com/fernandotherojo"
-            // @ts-expect-error react-native-web only types
-            hrefAttrs={{
-              target: '_blank',
-              rel: 'noreferrer',
-            }}
-            sx={{ color: 'blue' }}
-          >
-            Fernando Rojo
-          </A>
-          .
-        </P>
-      </View>
-      <View sx={{ height: 32 }} />
-      <Row>
-        <TextLink
-          href="/user/fernando"
-          textProps={{
-            style: sx({ fontSize: 16, fontWeight: 'bold', color: 'blue' }),
-          }}
-        >
-          Regular Link
-        </TextLink>
-        <View sx={{ width: 32 }} />
-        <MotiLink
-          href="/user/fernando"
-          animate={({ hovered, pressed }) => {
-            'worklet'
+  const [latestNews, setLatestNews] = useState<News[]>([])
 
-            return {
-              scale: pressed ? 0.95 : hovered ? 1.1 : 1,
-              rotateZ: pressed ? '0deg' : hovered ? '-3deg' : '0deg',
-            }
-          }}
-          from={{
-            scale: 0,
-            rotateZ: '0deg',
-          }}
-          transition={{
-            type: 'timing',
-            duration: 150,
-          }}
-        >
-          <Text
-            selectable={false}
-            sx={{ fontSize: 16, color: 'black', fontWeight: 'bold' }}
-          >
-            Moti Link
-          </Text>
-        </MotiLink>
-      </Row>
+  useEffect(() => {
+    getLatestNews().then((data) => {
+      setLatestNews(data)
+    })
+  }, [])
+
+  const renderItem: ListRenderItem<News> = ({ item }) => (
+    <View sx={{ paddingHorizontal: 16, marginBottom: 20 }}>
+      <View
+        sx={{
+          padding: 16,
+          borderWidth: 1,
+          borderColor: '#ddd',
+          borderRadius: 8,
+        }}
+      >
+        {item.imageUrl && (
+          <View sx={{ minHeight: 300, marginBottom: 16 }}>
+            <Image
+              source={{ uri: item.imageUrl }}
+              height={400}
+              width={800}
+              resizeMode={'cover'}
+              alt={item.title}
+              sx={{ flex: 1, borderRadius: 8 }}
+            />
+          </View>
+        )}
+        <TextLink href={`latest-news/${item.id}`}>
+          <H2 sx={{ color: '#444', fontSize: 18 }}>{item.title}</H2>
+        </TextLink>
+      </View>
+    </View>
+  )
+
+  return (
+    <View>
+      {Platform.OS === 'web' && (
+        <View sx={{ paddingHorizontal: 16 }}>
+          <H1 sx={{ marginBottom: 10 }}>Latest News</H1>
+        </View>
+      )}
+      <FlatList
+        sx={{ marginTop: 16 }}
+        data={latestNews}
+        renderItem={renderItem}
+        // keyExtractor={(item: News) => item.id}
+      />
     </View>
   )
 }
